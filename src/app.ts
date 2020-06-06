ab6842f92304b017b151d9a3ecdb4e00b32fa3a0
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-06-06 20:24:30 +0900

    Update cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import cors from "cors";
 import "./routes";
 import RootRouter from "./classes/RootRouter";
+import env from "./constants/env";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: env.NODE_ENV === "production" ? "https://sunrinexit.wtf" : "*",
+  })
+);
 app.use(RootRouter.getRouter());
 
 export default app;
--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,6 +1,7 @@
 const env = {
   PORT: process.env.PORT || 4000,
   MONGO_URI: process.env.MONGO_URI || "mongodb://mongo/sunrinside",
+  NODE_ENV: process.env.NODE_ENV || "development",
 };
 
 export default env;
