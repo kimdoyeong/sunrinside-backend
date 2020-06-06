2ce0ae80e7cda5f9703987a84e3bc181de86663f
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-06-06 18:58:24 +0900

    Update router error handling

--- src/classes/RootRouter.ts
+++ src/classes/RootRouter.ts
@@ -1,5 +1,6 @@
-import { Router } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import RouterClass from "../classes/Router";
+import { InternalServerError } from "../constants/errors/InternalServer";
 class RootRouter {
   private static router = Router();
 
@@ -8,7 +9,27 @@ class RootRouter {
     this.router.use(router.getRouter());
   }
   public static getRouter() {
+    this.router.use(this.errorHandler);
     return this.router;
+  }
+
+  private static errorHandler(
+    err: any,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    const error = err.code && err.status ? err : InternalServerError;
+    if (!error.code && !err.status) {
+      console.error(err);
+    }
+
+    res.status(err.status).json({
+      success: false,
+      message: err.message,
+      status: err.status,
+      code: err.code,
+    });
   }
 }
 
