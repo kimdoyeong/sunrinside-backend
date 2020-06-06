95cd0175b9e0a5eb36ba90160944e82e4cee63bb
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-06-06 19:33:27 +0900

    Update bodyParser

--- src/classes/RootRouter.ts
+++ src/classes/RootRouter.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response, NextFunction } from "express";
+import express, { Router, Request, Response, NextFunction } from "express";
 import RouterClass from "../classes/Router";
 import { InternalServerError } from "../constants/errors/InternalServer";
 import { ActionNotFound } from "../constants/errors/NotFound";
@@ -10,11 +10,13 @@ class RootRouter {
     this.router.use(router.getRouter());
   }
   public static getRouter() {
-    this.router.use((req, res, next) => {
+    const router = this.router;
+    router.use(express.json());
+    router.use((req, res, next) => {
       next(ActionNotFound);
     });
-    this.router.use(this.errorHandler);
-    return this.router;
+    router.use(this.errorHandler);
+    return router;
   }
 
   private static errorHandler(
