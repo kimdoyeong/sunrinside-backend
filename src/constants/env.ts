8748ca861cc33933d29fd258afd30323eecb9afc
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-06-08 16:19:45 +0900

    Update env option

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -3,6 +3,7 @@ const env = {
   MONGO_URI: process.env.MONGO_URI || "mongodb://mongo/sunrinside",
   NODE_ENV: process.env.NODE_ENV || "development",
   SITE_URL: process.env.SITE_URL || "http://localhost:3000",
+  IGNORE_EMAIL_HOST: process.env.IGNORE_EMAIL_HOST || false,
 };
 
 export default env;
