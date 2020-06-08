d1e67d72b9c6c89ad8c51d3c8945e8f75bd116af
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-06-08 18:55:20 +0900

    Update model Thread

---
+++ src/models/Thread.ts
@@ -0,0 +1,58 @@
+import { Schema, model, Document } from "mongoose";
+import User from "./User";
+
+const modelName = "thread";
+const threadSchema = new Schema({
+  title: {
+    type: String,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  by: {
+    type: Schema.Types.ObjectId,
+    ref: User.modelName,
+    required: true,
+  },
+  subthreads: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: modelName,
+    },
+  ],
+  medias: {
+    type: [String],
+    default: [],
+  },
+  vote: {
+    type: {
+      up: Number,
+      down: Number,
+    },
+    default: {
+      up: 0,
+      down: 0,
+    },
+  },
+});
+
+export interface IThread {
+  title?: string;
+  content: string;
+  by: string;
+  subthreads?: string[];
+  medias?: string[];
+}
+export interface ThreadDocument extends IThread, Document {
+  subthreads: string[];
+  medias: string[];
+  vote: {
+    up: number;
+    down: number;
+  };
+}
+
+const Thread = model<ThreadDocument>(modelName, threadSchema);
+
+export default Thread;
