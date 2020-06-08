63b62fa8d00ccab33cd79d7aa2d2da3a43c0d263
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-06-08 18:38:15 +0900

    Update put /user/:id/admin

--- src/routes/User/admin.ts
+++ src/routes/User/admin.ts
@@ -1,8 +1,26 @@
+import { Request } from "express";
 import Router from "../../classes/Router";
+import User from "../../models/User";
+import { UserNotFound } from "../../constants/errors/NotFound";
 
 class UserAdminRouter extends Router {
   constructor() {
     super();
+    this.register("put", "/:id/admin", this.setUserAdmin, { auth: "admin" });
+  }
+
+  async setUserAdmin(req: Request) {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+
+    if (!user) throw UserNotFound;
+    user.isAdmin = true;
+    await user.save();
+
+    return {
+      status: 200,
+      success: true,
+    };
   }
 }
 
